77333f90ee6f4980d2d41481a114321f42bb512a
Okami-Chen <support@616.icu>
2023-11-03 17:07:48 +0800

    * 修改页数

--- web/src/api/addons/fang/fang/index.ts
+++ web/src/api/addons/fang/fang/index.ts
@@ -2,6 +2,7 @@ import { http, jumpExport } from '@/utils/http/axios';
 
 // 获取租房列表
 export function List(params) {
+  params.pageSize = 15
   return http.request({
     url: '/fang/fang/list',
     method: 'get',
